3daac742f383d323449377d15b683413cc8debc4
Patric Thysell <patric.thysell@br-automation.com>
2026-08-05 14:23:14 +0200

    Using appropriate definition of ssize_t from stdio.h

--- src/sys_br.h
+++ src/sys_br.h
@@ -5,18 +5,13 @@
 #include <assert.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include <string.h>
 #include <inttypes.h>
 
 /* B&R headers */
 #include <bur/plctypes.h>
 #include "CAN_Lib.h"
-
-/* Define ssize_t as 32-bit for Automation Runtime */
-#if !defined(_SSIZE_T) && !defined(_SSIZE_T_DEFINED) && !defined(__ssize_t_defined) && !defined(_SSIZE_T_DECLARED)
-typedef DINT ssize_t;
-#define _SSIZE_T_DEFINED 1
-#endif
 
 /* CAN_Lib address fields use UDINT pointers. */
 #ifndef BRCAN_ADR
